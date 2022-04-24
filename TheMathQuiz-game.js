01d24d01e8066ae86dc65f87219e33e1afb61c45
jabirhaider25 <91767507+jabirhaider25@users.noreply.github.com>
2022-04-24 11:51:39 -0500

    Update TheMathQuiz-game.js

--- TheMathQuiz-game.js
+++ TheMathQuiz-game.js
@@ -26,3 +26,48 @@ row + question_number + input_box + check_button;
 document.getElementById("output").innerHTML = row;
 document.getElementById("number1").value = "";
 document.getElementById("number2").value = "";                                                                                       
+
+
+question_turn = "player1";
+answer_turn = "player2";
+
+
+if (question_turn == "player1") {
+    question_turn = "player2";
+    document.getElementById("player_question").innerHTML = "question_turn" + player2_name;
+} else {
+    question_turn = player2 + 1;
+    document.getElementById("player_question").innerHTML = "question_turn" + player1_name
+}
+
+
+function Check() {
+    get_answer = document.getElementById("input_check_box").value;
+   if (get_answer = actual_answer);
+    
+    if (answer == word) {
+
+        if (answer_turn == "player1") {
+            player1_score = player1_score + 1;
+            document.getElementById("player1_score").innerHTML = update_player1_score;
+        } else {
+            player2_score = player2_score + 1;
+            document.getElementById("player2_score").innerHTML = update_player2_score
+        }
+
+    }
+}
+
+
+
+if (answer_turn == "player1") {
+    answer_turn = "player2";
+    document.getElementById("player_answer").innerHTML = "answer_turn" + player2_name;
+} else {
+    question_turn = player2 + 1;
+    document.getElementById("player_answer").innerHTML = "answer_turn" + player1_name
+}
+
+
+document.getElementById("output").innerHTML = "";
+}
